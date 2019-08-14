3d23c0e592305d2ef86657eb03a6fb183c5fac1e
Andriy Lozynskiy <alozynskiy@gmail.com>
2019-08-14 11:24:06 +0300

    fix default image path

--- src/app/default-image.directive.ts
+++ src/app/default-image.directive.ts
@@ -1,10 +1,14 @@
 import {Directive, HostListener} from '@angular/core';
+import {environment} from '../environments/environment';
 
 @Directive({
     selector: 'img[src], img[attr.src]'
 })
 export class DefaultImageDirective {
     public defaultImg = 'assets/images/default.png';
+    constructor(){
+      this.defaultImg = environment.production ? 'ng/'+this.defaultImg : this.defaultImg;
+    }
 
     @HostListener('error', ['$event'])
     onError(event) {
